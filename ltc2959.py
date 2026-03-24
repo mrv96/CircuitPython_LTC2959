95cc23d1e0295de98d0c750271cfebbbc0921ee1
mrv96 <mrv96@users.noreply.github.com>
2026-03-24 09:55:08 +0100

    Initial commit

---
+++ ltc2959.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2026 mrv96
+#
+# SPDX-License-Identifier: MIT
+"""
+`ltc2959`
+================================================================================
+
+CircuitPython module for the LTC2959 ultra-low power battery gas gauge.
+
+
+* Author(s): mrv96
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/mrv96/CircuitPython_LTC2959.git"
